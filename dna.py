7cda3f7b786f0694f4045c6b7a2aa41e3db9bdf2
Nicole Tibay <neequole@gmail.com>
2019-07-15 22:09:07 +1000

    Add dna solutions

---
+++ dna.py
@@ -0,0 +1,28 @@
+""" http://rosalind.info/problems/dna/ """
+from collections import Counter
+
+nucleobases = {
+    "A": 0,
+    "C": 0,
+    "G": 0,
+    "T": 0,
+}
+
+with open('data/rosalind_dna.txt', 'r') as f1:
+    dna = f1.read()
+    # Using dictionary
+
+    for l in dna:
+        if l in nucleobases.keys():
+            nucleobases[l] += 1
+
+    print(" ".join(map(str, nucleobases.values())))
+
+    # Using built in count method
+    count_result = [dna.count(l) for l in nucleobases.keys()]
+    print(" ".join(map(str, count_result)))
+
+    # Using collections Counter
+    c = Counter(dna)
+    counter_result = [c[l] for l in nucleobases.keys()]
+    print(" ".join(map(str, counter_result)))
