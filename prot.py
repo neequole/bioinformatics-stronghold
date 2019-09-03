18a8d1fdc1a013e7a6f3c898d838d3f13102119b
Nicole Tibay <neequole@gmail.com>
2019-09-03 16:04:56 +1000

    Add protein solution

---
+++ prot.py
@@ -0,0 +1,83 @@
+# RNA codon table
+rna_codon = {
+    "UUU": "F",
+    "UUC": "F",
+    "UUA": "L",
+    "UUG": "L",
+    "UCU": "S",
+    "UCC": "S",
+    "UCA": "S",
+    "UCG": "S",
+    "UAU": "Y",
+    "UAC": "Y",
+    "UAA": "Stop",
+    "UAG": "Stop",
+    "UGU": "C",
+    "UGC": "C",
+    "UGA": "Stop",
+    "UGG": "W",
+    "CUU": "L",
+    "CUC": "L",
+    "CUA": "L",
+    "CUG": "L",
+    "CCU": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+    "CAU": "H",
+    "CAC": "H",
+    "CAA": "Q",
+    "CAG": "Q",
+    "CGU": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",
+    "AUU": "I",
+    "AUC": "I",
+    "AUA": "I",
+    "AUG": "M",
+    "ACU": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+    "AAU": "N",
+    "AAC": "N",
+    "AAA": "K",
+    "AAG": "K",
+    "AGU": "S",
+    "AGC": "S",
+    "AGA": "R",
+    "AGG": "R",
+    "GUU": "V",
+    "GUC": "V",
+    "GUA": "V",
+    "GUG": "V",
+    "GCU": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+    "GAU": "D",
+    "GAC": "D",
+    "GAA": "E",
+    "GAG": "E",
+    "GGU": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+}
+
+
+with open('data/rosalind_prot.txt', 'r') as f1:
+    dna = f1.read()
+    index = 0
+    protein = ""
+
+    for i in range(0, len(dna)-3, 3):
+        x = dna[i:i+3]
+        y = rna_codon[x]
+        if y == "Stop":
+            break
+        protein += y
+
+    print(protein)
+
